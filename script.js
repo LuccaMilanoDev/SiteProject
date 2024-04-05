f76a65463352e991e9d4b0a63f8889714e2d5fda
LuccaMilanoDev <luccamilano2018@gmail.com>
2024-04-05 20:46:40 -0300

    acrescentando coisas

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 function toggleMode() {
   const html = document.documentElement
-  if (html.classList.contains('light')) {
-    html.classList.remove('light')
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
   } else {
-    html.classList.add('light')
+    html.classList.add("light")
   }
 }
 
-
+function toggleModeSobre() {
+  const htmlSobre = document.documentElement
+  if (htmlSobre.classList.contains("lightSobre")) {
+    htmlSobre.classList.remove("lightSobre")
+  } else {
+    htmlSobre.classList.add("lightSobre")
+  }
+}
